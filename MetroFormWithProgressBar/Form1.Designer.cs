e1dc47c9f7e3516662b776a90440deaebfbe21f7
Robert Rozas Navarro <robert.rozas.n@gmail.com>
2018-03-23 10:30:46 -0300

    Barra de progreso que no bloquea el thread de la UI

---
+++ MetroFormWithProgressBar/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+
+namespace MetroFormWithProgressBar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new MetroFramework.Controls.MetroButton();
+            this.button2 = new MetroFramework.Controls.MetroButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new MetroFramework.Controls.MetroProgressBar();
+            this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Highlight = true;
+            this.button1.Location = new System.Drawing.Point(274, 332);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(169, 46);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Mensaje";
+            this.button1.UseSelectable = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Highlight = true;
+            this.button2.Location = new System.Drawing.Point(555, 332);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(169, 46);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Contador";
+            this.button2.UseSelectable = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Calibri", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(271, 131);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 50);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "0";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(71, 98);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(672, 23);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // backgroundWorker
+            // 
+            this.backgroundWorker.WorkerReportsProgress = true;
+            this.backgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker_DoWork_1);
+            this.backgroundWorker.ProgressChanged += backgroundWorker_ProgressChanged;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.ShadowType = MetroFramework.Forms.MetroFormShadowType.None;
+            this.Text = "Demo Barra Progreso";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroButton button1;
+        private MetroFramework.Controls.MetroButton button2;
+        private System.Windows.Forms.Label label1;
+        private MetroFramework.Controls.MetroProgressBar progressBar1;
+        public System.ComponentModel.BackgroundWorker backgroundWorker;
+    }
+}
+
---
+++ MetroFormWithProgressBar/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework;
+using MetroFramework.Forms;
+
+namespace MetroFormWithProgressBar
+{
+    public partial class Form1 : MetroForm
+    {
+        private int counter = 1;
+        public Form1()
+        {
+            InitializeComponent();
+            this.BorderStyle = MetroFormBorderStyle.FixedSingle;
+            this.ShadowType = MetroFormShadowType.AeroShadow;
+        }
+
+
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+            // Start the BackgroundWorker.
+            backgroundWorker.RunWorkerAsync();
+        }
+
+       
+        private void backgroundWorker_ProgressChanged(object sender,
+            ProgressChangedEventArgs e)
+        {
+            // Change the value of the ProgressBar to the BackgroundWorker progress.
+            progressBar1.Value = e.ProgressPercentage;
+            // Set the text.
+            this.Text = e.ProgressPercentage.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MetroMessageBox.Show(this, "This is a sample MetroMessagebox `OK` only button", "MetroMessagebox", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int labelNum = Convert.ToInt32(label1.Text);
+            label1.Text = (labelNum + this.counter).ToString();
+        }
+
+        private void backgroundWorker_DoWork_1(object sender, DoWorkEventArgs e)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                // Wait 100 milliseconds.
+                Thread.Sleep(100);
+                // Report progress.
+                backgroundWorker.ReportProgress(i);
+            }
+        }
+    }
+}
+
+
+
